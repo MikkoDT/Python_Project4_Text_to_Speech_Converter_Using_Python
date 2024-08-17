2f0436d12578d641271a9dc9bcd71f5614626d00
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-17 23:26:30 +0800

    update

--- code/convertfile_audio.py
+++ code/convertfile_audio.py
@@ -6,4 +6,4 @@ text = open('dem1.txt','r').read()
 language = 'en'
 output = gTTS(text=text,lang=language,slow=False)
 output.save('fileoutput.mp3')
-os.system("afplay fileoutput.mp3")
+os.system("fileoutput.mp3")
