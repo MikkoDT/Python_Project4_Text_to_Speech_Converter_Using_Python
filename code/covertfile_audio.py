92523a1a2ec991ba3305e21667c5cbc777f85e57
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-17 22:37:21 +0800

    Update covertfile_audio.py

--- code/covertfile_audio.py
+++ code/covertfile_audio.py
@@ -3,3 +3,7 @@ import os
 
 text = open('dem1.txt','r').read()
 
+language = 'en'
+output = gTTS(text=text,lang=language,slow=False)
+output.save('fileoutput.mp3')
+os.system("afplay fileoutput.mp3")
